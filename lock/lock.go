025311ba15d842d2346de3c89ea9dbd0602253b4
liz <rick.lizhan@gmail.com>
2020-01-01 20:30:27 +0800

    lock

---
+++ lock/lock.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"sync"
+)
+
+// 全局变量
+var counter int
+
+func main() {
+	noLock()
+}
+
+func noLock() {
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter++
+		}()
+	}
+	wg.Wait()
+	println(counter)
+
+}
