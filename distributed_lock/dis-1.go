f00ba3ddaf9775defd791e85ad4223d52d94916b
liz <rick.lizhan@gmail.com>
2020-01-14 22:09:42 +0800

    lock

--- distributed_lock/dis-1.go
+++ distributed_lock/dis-1.go
@@ -6,12 +6,33 @@ import "sync"
 var counter int
 
 func main() {
+	// notLock()
+	haveLock()
+}
+
+func notLock() {
 	var wg sync.WaitGroup
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			counter++
+		}()
+	}
+	wg.Wait()
+	println(counter)
+}
+
+func haveLock() {
+	var wg sync.WaitGroup
+	var l sync.Mutex
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			l.Lock()
+			counter++
+			l.Unlock()
 		}()
 	}
 	wg.Wait()
